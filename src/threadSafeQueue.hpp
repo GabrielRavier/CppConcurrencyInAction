a6e73fefcf165062a442966348792e973b775159
GabrielRavier <gabravier@gmail.com>
2019-01-08 21:14:04 +0100

    Did a few small things
    Signed-off-by: GabrielRavier <gabravier@gmail.com>

--- src/threadSafeQueue.hpp
+++ src/threadSafeQueue.hpp
@@ -11,7 +11,7 @@ template<typename T> class ThreadSafeQueue
 {
 private:
     mutable std::mutex m_mutex;
-    std::queue<T> m_dataQueue;
+    std::queue<std::shared_ptr<T>> m_dataQueue;
     std::condition_variable m_dataCond;
 
 public:
@@ -19,6 +19,7 @@ public:
     {
 
     }
+
     ThreadSafeQueue(const ThreadSafeQueue& other)
     {
         std::lock_guard<std::mutex> lock(other.m_mutex);
@@ -27,11 +28,12 @@ public:
 
     ThreadSafeQueue& operator=(const ThreadSafeQueue&) = delete; // Disallow assignment for simplicity
 
-    void push(T newVal)
+    void waitAndPop(T& value)
     {
-        std::lock_guard<std::mutex> lock(m_mutex);
-        m_dataQueue.push(std::move(newVal));
-        m_dataCond.notify_one();
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_dataCond.wait(lock, [this] {return !m_dataQueue.empty();});
+        value = std::move(*m_dataQueue.front());
+        m_dataQueue.pop();
     }
 
     // Store retrieved value in the referenced variable, retval to check for failure
@@ -40,9 +42,18 @@ public:
         std::lock_guard<std::mutex> lock(m_mutex);
         if (m_dataQueue.empty())
             return false;   // No value could be retrieved
-        val = std::move(m_dataQueue.front());
+        val = std::move(*m_dataQueue.front());
         m_dataQueue.pop();
         return true;
+    }
+
+    std::shared_ptr<T> waitAndPop()
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_dataCond.wait(lock, [this]{return !m_dataQueue.empty();});
+        auto result = m_dataQueue.front();
+        m_dataQueue.pop();
+        return result;
     }
 
     // Return nullptr if fail
@@ -51,26 +62,19 @@ public:
         std::lock_guard<std::mutex> lock(m_mutex);
         if (m_dataQueue.empty())
             return std::shared_ptr<T>();    // No value could be retrieved
-        auto result(std::make_shared<T>(std::move(m_dataQueue.front())));
+        auto result = m_dataQueue.front();
         m_dataQueue.pop();
         return result;
     }
 
-    void waitAndPop(T& value)
+    void push(T newVal)
     {
-        std::unique_lock<std::mutex> lock(m_mutex);
-        m_dataCond.wait(lock, [this] {return !m_dataQueue.empty();});
-        value = std::move(m_dataQueue.front());
-        m_dataQueue.pop();
-    }
-
-    std::shared_ptr<T> waitAndPop()
-    {
-        std::unique_lock<std::mutex> lock(m_mutex);
-        m_dataCond.wait(lock, [this]{return !m_dataQueue.empty();});    // 4
-        auto result(std::make_shared<T>(std::move(m_dataQueue.front())));
-        m_dataQueue.pop();
-        return result;
+        // Allocation can be done before getting the lock
+        auto ptr = std::make_shared<T>(std::move(newVal)));
+        
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_dataQueue.push(ptr);
+        m_dataCond.notify_one();
     }
 
     bool empty() const
